75b84727f86cfd908309ba07053f16deaef94496
dunitian <1054186320@qq.com>
2016-12-29 11:58:58 +0800

    水印第四版 ~ 非人水印（添加人脸识别）

--- Software/WaterMarkAPP/LoTWaterAPP/MainWindow.xaml.cs
+++ Software/WaterMarkAPP/LoTWaterAPP/MainWindow.xaml.cs
@@ -29,91 +29,115 @@ namespace WaterWaterWaterMark
 
         }
         #region 公用方法
+
+        #region 人脸识别-水印
         /// <summary>
         /// 设置水印--识别人脸
         /// </summary>
-        /// <param name="imgPaths"></param>
-        /// <param name="waterImgPath"></param>
+        /// <param name="imgPaths">原图路径</param>
+        /// <param name="waterImgPath">水印图路径</param>
+        /// <param name="savePath">保存路径</param>
+        /// <param name="thumPath">水印路径</param>
+        /// <param name="facePath">人脸路径</param>
         /// <returns></returns>
-        private int SetWaterMarkFace(string[] imgPaths, string waterImgPath, string savePath)
+        private void SetWaterMarkFace(string[] imgPaths, string waterImgPath, string savePath, string thumPath, string facePath)
         {
-            int count = 0;
-
             for (int k = 0; k < imgPaths.Length; k++)
             {
+                string imgPath = imgPaths[k];
                 //文件名
-                string fileName = Path.GetFileName(imgPaths[k]);
-                try
+                string fileName = Path.GetFileName(imgPath);
+
+                #region 生成缩略图
+                //缩略图图片路径
+                string thumImagePath = $"{thumPath}\\{fileName}";
+                //为了提高网络传输速度：V2会先生成缩略图再上传
+                using (var smallImg = new MagickImage(imgPath))
                 {
-                    #region 水印
-                    var list = FaceHelper.GetFaceModelList(imgPaths[k]).Result;
+                    smallImg.Resize(smallImg.Width / 2, smallImg.Height / 2);
+                    smallImg.Write(thumImagePath);
+                }
+                #endregion
 
-                    //原图
-                    using (var image = new MagickImage(imgPaths[k]))
+                //获取脸部信息
+                var task = FaceHelper.GetFaceModelList(thumImagePath);//Task.Factory.StartNew(() => FaceHelper.GetFaceModelList(thumImagePath));
+                var list = task.Result;
+
+                #region 水印
+                //原图
+                using (var image = new MagickImage(imgPath))
+                {
+                    int imgWidth = image.Width;
+                    int imgHeight = image.Height;
+
+                    #region 单个水印图
+                    using (var waterimg = new MagickImage(waterImgPath))
                     {
-                        int imgWidth = image.Width;
-                        int imgHeight = image.Height;
+                        int smallWidth = waterimg.Width;
+                        int smallHeight = waterimg.Height;
 
-                        #region 单个水印图
-                        using (var waterimg = new MagickImage(waterImgPath))
+                        int x = Convert.ToInt32(Math.Ceiling(imgWidth * 1.0 / smallWidth));
+                        int y = Convert.ToInt32(Math.Ceiling(imgHeight * 1.0 / smallHeight));
+
+                        //透明度（1~100，越大水印越淡）
+                        waterimg.Evaluate(Channels.Alpha, EvaluateOperator.Divide, o);
+                        for (int i = 0; i < x; i++)
                         {
-                            int smallWidth = waterimg.Width;
-                            int smallHeight = waterimg.Height;
-
-                            int x = Convert.ToInt32(Math.Ceiling(imgWidth * 1.0 / smallWidth));
-                            int y = Convert.ToInt32(Math.Ceiling(imgHeight * 1.0 / smallHeight));
-
-                            //透明度（1~100，越大水印越淡）
-                            waterimg.Evaluate(Channels.Alpha, EvaluateOperator.Divide, o);
-                            for (int i = 0; i < x; i++)
+                            for (int j = 0; j < y; j++)
                             {
-                                for (int j = 0; j < y; j++)
-                                {
-                                    image.Composite(waterimg, i * smallWidth, j * smallHeight, CompositeOperator.Over);//水印
-                                }
+                                image.Composite(waterimg, i * smallWidth, j * smallHeight, CompositeOperator.Over);//水印
                             }
                         }
-                        #endregion
+                    }
+                    #endregion
 
-                        #region 裁剪脸部并还原 2016-12-26
-                        //原图
-                        using (var magickImg = new MagickImage(imgPaths[k]))
+                    #region 裁剪脸部并还原 2016-12-26
+                    //识别不出来脸就不还原
+                    if (list != null && list.Count() > 0)
+                    {
+                        //原图 （大小*2）
+                        foreach (var item in list)
                         {
-                            foreach (var item in list)
+                            using (var magickImg = new MagickImage(imgPath))
                             {
                                 #region 调整信息
                                 if (item.FaceRectangLe == null)
                                     continue;
+                                //尺寸还原
+                                int width = item.FaceRectangLe.Width * 2;
+                                int height = item.FaceRectangLe.Height * 2;
+                                int x = item.FaceRectangLe.Left * 2;
+                                int y = item.FaceRectangLe.Top * 2;
+
                                 //在原有基础上宽高各*2（PS:我们所谓的不水印脸上其实更多意义在"头"）
-                                int addWidth = item.FaceRectangLe.Width / 2;
-                                int addHeight = item.FaceRectangLe.Height / 2;
                                 //新的宽高
-                                item.FaceRectangLe.Width *= 2;
-                                item.FaceRectangLe.Height *= 2;
+                                width *= 2;
+                                height *= 2;
                                 //新的左顶点坐标（newX,newY）【PS:不用担心负数问题，Model已经约束了】
-                                item.FaceRectangLe.Left -= addWidth;
-                                item.FaceRectangLe.Top -= addHeight;
+                                x -= item.FaceRectangLe.Width;
+                                y -= item.FaceRectangLe.Height;
                                 #endregion
+
                                 //裁剪
-                                magickImg.Crop(item.FaceRectangLe.Left, item.FaceRectangLe.Top, item.FaceRectangLe.Width, item.FaceRectangLe.Height);
-                                //还原脸
-                                image.Composite(magickImg, item.FaceRectangLe.Left, item.FaceRectangLe.Top, CompositeOperator.Over);
+                                magickImg.Crop(x, y, width, height);
+                                //保存一下脸（到时候可以查看误差）[一张图可能多个脸]
+                                magickImg.Write(string.Format(@"{0}\{1}{2}", facePath, item.FaceId, fileName));
+                                //还原脸（Image是水印图的Magick对象）
+                                image.Composite(magickImg, x, y, CompositeOperator.Over);
                             }
                         }
-                        #endregion
-
-                        image.Write(string.Format(@"{0}\{1}", savePath, fileName));
-                        count++;
                     }
                     #endregion
+
+                    image.Write(string.Format(@"{0}\{1}", savePath, fileName));
                 }
-                catch (Exception ex)
-                {
-                    File.AppendAllText("Images/dnt.log", ex.ToString());
-                }
+                #endregion
+
             }
-            return count;
         }
+        #endregion
+
+        #region 普通方式-水印
         /// <summary>
         /// 设置水印
         /// </summary>
@@ -163,12 +187,17 @@ namespace WaterWaterWaterMark
                 }
                 catch (Exception ex)
                 {
-                    File.AppendAllText("Images/dnt.log", ex.ToString());
+                    lock (this)
+                    {
+                        File.AppendAllText("Images/dnt.log", ex.ToString());
+                    }
                 }
             }
             return count;
         }
+        #endregion
 
+        #region 批量水印
         /// <summary>
         /// 批量水印
         /// </summary>
@@ -191,6 +220,7 @@ namespace WaterWaterWaterMark
                 var listFiles = files.Where(f => f.ToLower().Contains(".png") || f.ToLower().Contains(".jpg") || f.ToLower().Contains(".bmp") || f.ToLower().Contains(".gif") || f.ToLower().Contains(".jpeg"));
                 if (listFiles == null || listFiles.Count() < 1) { MessageBox.Show("该目录木有png，jpg，bmp，gif之类的常用图片格式", "逆天友情提醒"); return; }
                 files = listFiles.ToArray();
+
                 #region 存储专用
                 //图片所处目录
                 string dirPath = Path.GetDirectoryName(files[0]);
@@ -202,15 +232,45 @@ namespace WaterWaterWaterMark
                     Directory.CreateDirectory(savePath);
                 }
                 #endregion
+
                 var result = MessageBox.Show(string.Format("总共识别出 {0} 张图片，操作进行中~~~\n\n是否人脸识别？（请节约使用，每个月使用次数都是有限的）\n\n你可以先去做其他事情，一会再来看看~（人脸识别比较慢）", files.Length), "逆天友情提醒~~~没有人物的就不要人脸识别了！", MessageBoxButton.YesNoCancel);
                 //人脸识别
                 if (result == MessageBoxResult.Yes)
                 {
-                    Task.Factory.StartNew(() => SetWaterMarkFace(files, path, savePath));
+                    #region 是否存在，不存在就创建（缩略图，脸）
+                    string thumPath = $"{savePath}\\thum";
+                    if (!Directory.Exists(thumPath))
+                    {
+                        Directory.CreateDirectory(thumPath);
+                    }
+                    string facePath = $"{savePath}\\face";
+                    if (!Directory.Exists(facePath))
+                    {
+                        Directory.CreateDirectory(facePath);
+                    }
+                    #endregion
+
+                    var task = Task.Run(() => SetWaterMarkFace(files, path, savePath, thumPath, facePath));
+                    task.ContinueWith(t =>
+                    {
+                        if (t.IsFaulted)
+                        {
+                            var ex = t.Exception.GetBaseException();
+                            MessageBox.Show(ex.Message);
+                            lock (this)
+                            {
+                                File.AppendAllText("Images/dnt.log", ex.ToString());
+                            }
+                        }
+                        if (t.IsCompleted)
+                        {
+                            MessageBox.Show("thum目录是缩略图，可以直接删了\n\nface目录是识别出来的人脸，可以看看再删", "我已经完成了，您老看看呗~");
+                        }
+                    });
                 }
                 else if (result == MessageBoxResult.No)
                 {
-                    Task.Factory.StartNew(() => SetWaterMark(files, path, savePath));
+                    Task.Run(() => SetWaterMark(files, path, savePath));
                 }
                 if (result != MessageBoxResult.Cancel)
                 {
@@ -219,6 +279,9 @@ namespace WaterWaterWaterMark
                 }
             }
         }
+        #endregion
+
+        #region 配置文件
         /// <summary>
         /// 配置文件
         /// </summary>
@@ -259,6 +322,7 @@ namespace WaterWaterWaterMark
             }
             #endregion
         }
+        #endregion
         #endregion
 
         #region 按钮事件
--- Software/WaterMarkAPP/LoTWaterAPP/Properties/AssemblyInfo.cs
+++ Software/WaterMarkAPP/LoTWaterAPP/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("逆天批量水印 V1.2.1")]
+[assembly: AssemblyTitle("逆天批量水印 V1.2.3")]
 [assembly: AssemblyDescription("本次添加人脸识别功能，Key失效请联系我")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("逆天有限公司")]
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
